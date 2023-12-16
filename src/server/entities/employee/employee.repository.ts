e9f0dbe6a7371ac6721a7f8173032d39dbfb49cf
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 20:51:51 -0300

    Remove updateDepartment method from IEmployeeRepository interface

--- src/server/entities/employee/employee.repository.ts
+++ src/server/entities/employee/employee.repository.ts
@@ -1,9 +1,4 @@
 import { type Employee } from "~/server/entities/employee/employee.entity";
 import { type Repository } from "~/server/repositories/ports/repository.interface";
 
-export interface IEmployeeRepository extends Repository<Employee> {
-  updateDepartment(input: {
-    id: number;
-    departmentId: number;
-  }): Promise<Employee>;
-}
+export interface IEmployeeRepository extends Repository<Employee> {}
