209c16e5e0dfeaa293d175448157b1bee922c7ff
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 14:05:01 -0300

    add image domain to Next.js config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  images: {
+    domains: ["via.placeholder.com"],
+  },
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
