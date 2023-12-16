bc43355cb53ddc947b80edd8d321518d5c881628
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 20:46:39 -0300

    Update schema path in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
+  schema: "./src/server/repositories/drizzle/schema.ts",
   driver: "mysql2",
   dbCredentials: {
     connectionString: env.DATABASE_URL,
