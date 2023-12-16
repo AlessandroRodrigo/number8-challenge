1158c26bb1cd229d32a38e0d2ef6d9f85c87e91e
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 08:52:23 -0300

    update drizzle configuration and database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,5 @@ export default {
   dbCredentials: {
     connectionString: env.DATABASE_URL,
   },
-  tablesFilter: ["number8-challenge_*"],
+  tablesFilter: ["*"],
 } satisfies Config;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,13 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import { env } from "~/env";
+import * as schema from "./schema";
 
 const connection = await mysql.createConnection({
   uri: env.DATABASE_URL,
 });
 
-export const db = drizzle(connection);
+export const db = drizzle(connection, {
+  schema,
+  mode: "default",
+});
