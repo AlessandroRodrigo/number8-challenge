fcb3ba304f776b33702a0d94595ff8a3e19410f8
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 09:04:18 -0300

    add test script and configure vite-tsconfig-paths plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,14 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     env: {
       DATABASE_URL: "mysql://user:1234@localhost:3306/mydatabase",
+    },
+    alias: {
+      "~": "src",
     },
   },
 });
