69b8d4726bba9fd09937b40cc6d39fabba84a5a7
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 08:08:13 -0300

    update database connection to use environment variable

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,9 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
+import { env } from "~/env";
 
 const connection = await mysql.createConnection({
-  host: "host",
-  user: "user",
-  database: "database",
+  uri: env.DATABASE_URL,
 });
 
 export const db = drizzle(connection);
