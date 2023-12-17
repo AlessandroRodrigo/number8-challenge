5be590d0b35921d4a67deca26d3d133b2ff1b118
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 23:04:42 -0300

    Refactor function names in populate-database test file

--- src/server/__tests__/populate-database.ts
+++ src/server/__tests__/populate-database.ts
@@ -1,9 +1,9 @@
 import { setup } from "~/server/__tests__/setup-integration";
 
 export async function populate() {
-  const { prepareEmployeeWithDepartment } = setup();
+  const { createEmployeeWithDepartment } = setup();
 
   for (let i = 0; i < 10; i++) {
-    void prepareEmployeeWithDepartment();
+    void createEmployeeWithDepartment();
   }
 }
