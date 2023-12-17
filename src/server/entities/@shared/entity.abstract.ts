c586eed77629145231a32832c5d6554830c8dc3b
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-17 19:41:07 -0300

    Fix access modifier for notification property

--- src/server/entities/@shared/entity.abstract.ts
+++ src/server/entities/@shared/entity.abstract.ts
@@ -2,7 +2,7 @@ import Notification from "~/server/entities/@shared/notification/notification";
 
 export abstract class Entity {
   public id = 0;
-  protected notification: Notification;
+  public notification: Notification;
 
   constructor() {
     this.notification = new Notification();
