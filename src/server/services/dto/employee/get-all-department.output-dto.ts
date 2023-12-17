912abf7075e05d486dfd1ccf33e009ea90c38ab5
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-17 13:51:28 -0300

    Add GetAllDepartmentOutputDto and GetDepartmentOutputDto

---
+++ src/server/services/dto/employee/get-all-department.output-dto.ts
@@ -0,0 +1,3 @@
+import { GetDepartmentRegistryOutputDto } from "~/server/services/dto/employee/get-department-registry.output-dto";
+
+export const GetAllDepartmentOutputDto = GetDepartmentRegistryOutputDto.array();
