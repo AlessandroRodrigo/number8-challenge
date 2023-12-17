4742f62a87d9bd22502d0c94a95860b4f3758042
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-17 19:04:30 -0300

    Refactor employee details page

--- src/pages/employees/[id].tsx
+++ src/pages/employees/[id].tsx
@@ -1,56 +1,45 @@
-import { ActionIcon, Box, Container, Flex, Loader, Title } from "@mantine/core";
+import { ActionIcon, Box, Container, Flex, Title } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { EmployeeDepartmentRegistry } from "~/components/employee-department-registry";
 import { EmployeeDetailCard } from "~/components/employee-detail-card";
-import { api } from "~/utils/api";
+import { EmployeeDetailProvider } from "~/contexts/employee-detail.context";
 
 export default function EmployeeDetails() {
   const { query } = useRouter();
 
-  const { isLoading, data: employeeData } = api.employees.getById.useQuery(
-    {
-      id: Number(query.id),
-    },
-    {
-      enabled: !!query.id,
-    },
-  );
-
-  const employeeFullName = `${employeeData?.firstName} ${employeeData?.lastName}`;
-
-  if (isLoading) return <Loader />;
-
   return (
     <>
       <Head>
-        <title>{employeeFullName} - Employee Details</title>
+        <title>Employee Details</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container component="main" pb="lg">
-        <Flex align="baseline" gap="md">
-          <Link href="/" passHref>
-            <ActionIcon variant="transparent" color="dark">
-              <IconArrowLeft size={20} />
-            </ActionIcon>
-          </Link>
-          <Title order={1} py="lg">
-            {employeeFullName} details
-          </Title>
-        </Flex>
+      <EmployeeDetailProvider id={Number(query.id)}>
+        <Container component="main" pb="lg">
+          <Flex align="baseline" gap="md">
+            <Link href="/" passHref>
+              <ActionIcon variant="transparent" color="dark">
+                <IconArrowLeft size={20} />
+              </ActionIcon>
+            </Link>
+            <Title order={1} py="lg">
+              Employee details
+            </Title>
+          </Flex>
 
-        {employeeData && <EmployeeDetailCard {...employeeData} />}
+          <EmployeeDetailCard />
 
-        <Box mt="lg">
-          <Title order={3} mb="sm">
-            Employee's department history
-          </Title>
+          <Box mt="lg">
+            <Title order={3} mb="sm">
+              Employee's department history
+            </Title>
 
-          {employeeData && <EmployeeDepartmentRegistry id={employeeData.id} />}
-        </Box>
-      </Container>
+            <EmployeeDepartmentRegistry />
+          </Box>
+        </Container>
+      </EmployeeDetailProvider>
     </>
   );
 }
