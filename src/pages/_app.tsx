5fb3af359251f71f2afb22912e171add64baf618
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-17 15:47:02 -0300

    Add Mantine notifications component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
 import { createTheme, MantineProvider } from "@mantine/core";
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 
+import { Notifications } from "@mantine/notifications";
 import { api } from "~/utils/api";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,6 +15,7 @@ const theme = createTheme({});
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <MantineProvider theme={theme}>
+      <Notifications />
       <main className={inter.className}>
         <Component {...pageProps} />
       </main>
