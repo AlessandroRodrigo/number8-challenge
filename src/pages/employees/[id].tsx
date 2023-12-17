45e1d41886241740fdd98f43ecc426472cf4709a
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-17 16:09:37 -0300

    Refactor employee details page

--- src/pages/employees/[id].tsx
+++ src/pages/employees/[id].tsx
@@ -1,22 +1,19 @@
 import {
   ActionIcon,
-  Button,
+  Box,
   Card,
   Container,
   Flex,
-  Select,
-  Stack,
   Text,
   Title,
 } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 import { IconArrowLeft } from "@tabler/icons-react";
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { EmployeeCardUtils } from "~/components/employee-card/employee-card.utils";
+import { EmployeeDetailCard } from "~/components/employee-detail-card";
 import { api } from "~/utils/api";
 
 export default function EmployeeDetails() {
@@ -46,7 +43,7 @@ export default function EmployeeDetails() {
 
   const { isLoading: isUpdating, mutateAsync: updateEmployee } =
     api.employees.update.useMutation({
-      onSuccess(data, variables, context) {
+      onSuccess() {
         void refetchEmployeeData();
       },
     });
@@ -101,95 +98,18 @@ export default function EmployeeDetails() {
           </Title>
         </Flex>
 
-        <Card padding="lg" radius="md" withBorder>
-          <Flex gap="md">
-            <Image
-              width={250}
-              height={250}
-              src="https://via.placeholder.com/500"
-              alt="placeholder-image"
-            />
+        {employeeData && <EmployeeDetailCard {...employeeData} />}
 
-            <Flex direction="column" gap="md">
-              <Text size="lg" fw="bold">
-                {employeeFullName}
-              </Text>
+        <Box mt="lg">
+          <Title order={3} mb="sm">
+            Employee's department history
+          </Title>
 
-              <Flex direction="column" className="text-sm text-gray-700">
-                <span>Employee ID: {employeeData?.id}</span>
-                <span>Department: {employeeData?.department?.name}</span>
-                <span>Telephone: {employeeData?.phone}</span>
-                <span>Address: {employeeData?.address}</span>
-              </Flex>
-
-              <div className="flex flex-col gap-2">
-                <span>Update department</span>
-                <DepartmentSelect
-                  value={currentDepartment}
-                  onChange={(value) => setCurrentDepartment(value)}
-                />
-                <Button
-                  onClick={handleDepartmentChange}
-                  loading={isUpdating}
-                  disabled={departmentHasChanged}
-                >
-                  Update
-                </Button>
-              </div>
-            </Flex>
-
-            <Stack ml="auto">
-              <Title order={5}>Hire Date</Title>
-              <Stack className="flex flex-col">
-                <Flex gap="sm">
-                  <span className="text-sm text-gray-700">
-                    {EmployeeCardUtils.formatDate(employeeData?.hireDate)}
-                  </span>
-                  <span>
-                    ({EmployeeCardUtils.timeOfService(employeeData?.hireDate)})
-                  </span>
-                </Flex>
-
-                <Button
-                  onClick={handleToggleStatus}
-                  color={employeeData?.status === "active" ? "red" : "teal"}
-                >
-                  {employeeData?.status === "active"
-                    ? "Deactivate"
-                    : "Activate"}
-                </Button>
-              </Stack>
-            </Stack>
-          </Flex>
-        </Card>
+          <Card withBorder>
+            <Text>Table</Text>
+          </Card>
+        </Box>
       </Container>
     </>
-  );
-}
-
-type DepartmentSelectProps = {
-  value?: string;
-  onChange?: (value: string) => void;
-};
-
-function DepartmentSelect({ value, onChange }: DepartmentSelectProps) {
-  const { isLoading, data } = api.departments.getAll.useQuery();
-
-  if (isLoading) return <div>Loading...</div>;
-
-  return (
-    <Select
-      value={value}
-      onChange={(value) => {
-        if (!value) return;
-        onChange?.(value);
-      }}
-      data={
-        data?.map((department) => ({
-          value: department.id.toString(),
-          label: department.name,
-        })) ?? []
-      }
-    />
   );
 }
