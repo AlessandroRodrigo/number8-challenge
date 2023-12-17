3e8bc758f822affda7f9bb6ac1b474bfe1a530e0
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-16 22:33:13 -0300

    Update repositories in setup-integration test

--- src/server/__tests__/setup-integration.ts
+++ src/server/__tests__/setup-integration.ts
@@ -2,30 +2,18 @@ import { faker } from "@faker-js/faker";
 import { appRouter } from "~/server/api/root";
 import { createInnerTRPCContext } from "~/server/api/trpc";
 import { EmployeeFactory } from "~/server/entities/employee/employee.factory";
-import { InMemoryDepartmentRepository } from "~/server/repositories/in-memory/department.repository";
-import { InMemoryEmployeeRepository } from "~/server/repositories/in-memory/employee.repository";
+import { drizzleClient } from "~/server/repositories/drizzle/client";
+import { DepartmentRepository } from "~/server/repositories/drizzle/department.repository";
+import { EmployeeRepository } from "~/server/repositories/drizzle/employee.repository";
+import { department, employees } from "~/server/repositories/drizzle/schema";
 
-const departmentRepository = new InMemoryDepartmentRepository();
-const employeeRepository = new InMemoryEmployeeRepository();
+const departmentRepository = new DepartmentRepository();
+const employeeRepository = new EmployeeRepository();
 
 async function createDepartment() {
   return await departmentRepository.create({
     name: faker.commerce.department(),
   });
-}
-
-async function createEmployeeWithoutDepartment() {
-  const employee = EmployeeFactory.create({
-    id: 0,
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    hireDate: faker.date.past(),
-    phone: faker.phone.number(),
-    address: faker.location.streetAddress(),
-    department: null,
-  });
-
-  return employeeRepository.create(employee);
 }
 
 async function createEmployeeWithDepartment() {
@@ -48,8 +36,10 @@ async function createEmployeeWithDepartment() {
 }
 
 async function cleanDatabase() {
-  departmentRepository.clean();
-  employeeRepository.clean();
+  await Promise.all([
+    drizzleClient.delete(employees),
+    drizzleClient.delete(department),
+  ]);
 }
 
 export function setup() {
@@ -61,7 +51,6 @@ export function setup() {
     ctx,
     createDepartment,
     cleanDatabase,
-    createEmployeeWithoutDepartment,
     createEmployeeWithDepartment,
     departmentRepository,
     employeeRepository,
