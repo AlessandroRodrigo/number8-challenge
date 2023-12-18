1ffbd53fb5d8e6430e535c812bb25138ed1c94aa
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-17 23:07:34 -0300

    Remove populate-database test file

--- src/server/__tests__/populate-database.ts
+++
@@ -1,9 +0,0 @@
-import { setup } from "~/server/__tests__/setup-integration";
-
-export async function populate() {
-  const { createEmployeeWithDepartment } = setup();
-
-  for (let i = 0; i < 10; i++) {
-    void createEmployeeWithDepartment();
-  }
-}
