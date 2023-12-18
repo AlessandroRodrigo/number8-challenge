f7a076c25a15f115ceaf98b6462e9432b14b1a65
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-18 14:00:13 -0300

    Add ErrorBoundary component

---
+++ src/components/error-boundary/index.tsx
@@ -0,0 +1,48 @@
+import { Component, type ErrorInfo, type ReactNode } from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
+  errorInfo: ErrorInfo | null;
+}
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({
+      hasError: true,
+      error,
+      errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong!</h1>
+          <p>{this.state.error?.toString()}</p>
+          <div>Component Stack Trace:</div>
+          <pre>{this.state.errorInfo?.componentStack}</pre>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
