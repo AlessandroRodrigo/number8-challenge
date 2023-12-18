4e58a1d0dd37f24c48f956422d9413fa5c76ec64
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-18 13:40:10 -0300

    Add helper functions and refactor employee E2E tests

--- cypress/e2e/employee.cy.ts
+++ cypress/e2e/employee.cy.ts
@@ -1,3 +1,16 @@
+function waitForResponse(alias: string, statusCode: number) {
+  cy.wait(alias).then((interception) => {
+    expect(interception.response.statusCode).to.equal(statusCode);
+  });
+}
+
+function selectRandomElement(selector: string) {
+  cy.get(selector).then(($elements) => {
+    const randomIndex = Math.floor(Math.random() * $elements.length);
+    cy.wrap($elements).eq(randomIndex).click();
+  });
+}
+
 describe("Employee E2E", () => {
   beforeEach(() => {
     cy.intercept("GET", "**employees.getAll**").as("getEmployees");
@@ -10,15 +23,13 @@ describe("Employee E2E", () => {
   });
 
   it("should render a list of employees", () => {
-    cy.wait("@getEmployees").then((interception) => {
-      expect(interception.response.statusCode).to.equal(200);
-    });
+    waitForResponse("@getEmployees", 200);
 
     cy.get("article").should("have.length.greaterThan", 0);
   });
 
   it("should access the employee details page", () => {
-    cy.wait("@getEmployees");
+    waitForResponse("@getEmployees", 200);
 
     cy.get("article").first().contains("View details").click();
 
@@ -26,37 +37,30 @@ describe("Employee E2E", () => {
   });
 
   it("should change an employee status", () => {
-    cy.wait("@getEmployees");
+    waitForResponse("@getEmployees", 200);
 
-    cy.get("article").first().contains("View details").click();
+    selectRandomElement("button:contains('View details')");
 
-    cy.wait("@getEmployee");
+    waitForResponse("@getEmployee", 200);
 
     cy.get("button[role='toggle-status']").click();
 
-    cy.wait("@updateEmployee").then((interception) => {
-      expect(interception.response.statusCode).to.equal(200);
-    });
+    waitForResponse("@updateEmployee", 200);
   });
 
   it("should change the employee department", () => {
-    cy.wait("@getEmployees");
+    waitForResponse("@getEmployees", 200);
 
-    cy.get("article").first().contains("View details").click();
+    selectRandomElement("button:contains('View details')");
 
-    cy.wait("@getEmployee");
+    waitForResponse("@getEmployee", 200);
 
     cy.get("label").contains("Select a department").click();
 
-    cy.get("div[role='option']").then(($elements) => {
-      const randomIndex = Math.floor(Math.random() * $elements.length);
-      cy.wrap($elements).eq(randomIndex).click();
-    });
+    selectRandomElement("div[role='option']");
 
     cy.get("button").contains("Update").click();
 
-    cy.wait("@updateEmployee").then((interception) => {
-      expect(interception.response.statusCode).to.equal(200);
-    });
+    waitForResponse("@updateEmployee", 200);
   });
 });
